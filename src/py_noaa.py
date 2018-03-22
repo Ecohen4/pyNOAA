10fafeb53907f7b06157e07008453b2a2c35d213
Ecohen4 <ec2977@columbia.edu>
2018-03-22 15:49:46 -0600

    fixed date iteration bug

--- src/py_noaa.py
+++ src/py_noaa.py
@@ -1,11 +1,9 @@
-import os
 import json
 import pymongo
 import requests
 import pandas as pd
 from pprint import pprint
 
-# import pymongo_helpers
 
 class NoaaApi(object):
     '''
@@ -22,37 +20,51 @@ class NoaaApi(object):
         self.headers = {'token': api_key}
         self.endpoint = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?"
         self.payload = self._default_query_params()
-        # self._init_mongo_client()
 
-    def _update_payload(self, query_params):
-        self.payload.update(**query_params)
-
-    def _default_query_params(self):
-        return {
-        'datasetid': 'GSOM',
-        'locationid': 'ZIP:80435',
-        'startdate': pd.to_datetime('2013-10-06'),
-        'enddate': pd.to_datetime('2016-11-11'),
-        'limit': 1000,
-        'offset': 0
-        }
-
-    def _make_request(self, query_params=None):
+    def get_data(self, query_params=None):
         if query_params is not None:
             self._update_payload(query_params)
+        if (self.payload['enddate'] - self.payload['startdate']).days >= 365:
+            self._iterate_over_years()
+        else:
+            self._iterate_over_pages()
+
+    def _iterate_over_years(self):
+        year_range = pd.date_range(
+            start=self.payload['startdate'],
+            end=self.payload['enddate'],
+            freq='12M'
+        )
+        for i, year in enumerate(year_range[:-1]):
+            self.payload.update(
+            {'startdate': year_range[i].date(),
+            'enddate': year_range[i+1].date()
+            })
+            print('------------------------------')
+            print('Requesting data from {} to {}'.format(
+            self.payload['startdate'], self.payload['enddate'])
+            )
+            print('------------------------------')
+            self._iterate_over_pages()
+
+    def _iterate_over_pages(self, max_pages=100):
+        for i in range(max_pages):
+            print('requesting page {}...'.format(i))
+            self.payload.update({'offset': 1000*i})
+            response = self._make_request()
+            if self._valid_response(response):
+                data_ = self._parse_response(response)
+                self._insert_documents_into_db(data_)
+                if self._iteration_complete(data_):
+                    break
+                else:
+                    continue
+            else:
+                continue
+
+    def _make_request(self):
         response = requests.get(self.endpoint, headers=self.headers, params=self.payload)
         return response
-
-    # def _parse_response(self, response):
-    #     try:
-    #         list_of_dicts = response.json()['results']
-    #         print('records returned: {n}'.format(n=len(list_of_dicts)))
-    #         df = pd.DataFrame(list_of_dicts).set_index('station')
-    #     except (json.JSONDecodeError, KeyError) as e:
-    #         print('ERROR: {}'.format(e))
-    #         df = pd.DataFrame()
-    #     print(df)
-    #     return df
 
     def _valid_response(self, response):
         if response.status_code == 200:
@@ -67,9 +79,47 @@ class NoaaApi(object):
                 return False
         else:
             print ('WARNING: request failed with status code {}'.format(response.status_code))
+            print(response.url)
 
     def _parse_response(self, response):
         return response.json()['results']
+
+    def _iteration_complete(self, data_):
+        df = self._convert_to_df(data_)
+        n_records = len(df)
+        oldest_date = df.sort_values('date').iloc[0].date.split('T')[0]
+        most_recent_date = df.sort_values('date').iloc[-1].date.split('T')[0]
+        print('number of records: {n}'.format(n=n_records))
+        print('oldest record: {date}'.format(date=oldest_date))
+        print('most recent record: {date}'.format(date=most_recent_date))
+        if (most_recent_date == self.payload['enddate']):
+            print('Reached end of chunked date range')
+            return True
+        elif (n_records < 1000):
+            print('No more records to retrieve')
+            return True
+        else:
+            return False
+
+    def _update_payload(self, query_params):
+        self.payload.update(**query_params)
+        self._convert_datetime_to_date_only()
+
+    def _default_query_params(self):
+        return {
+        'datasetid': 'GSOM',
+        'locationid': 'ZIP:80435',
+        'startdate': pd.to_datetime('2013-10-06').date(),
+        'enddate': pd.to_datetime('2016-11-11').date(),
+        'limit': 1000,
+        'offset': 0
+        }
+
+    def _convert_datetime_to_date_only(self):
+        self.payload.update({
+        'startdate': pd.to_datetime(self.payload['startdate']).date(),
+        'enddate': pd.to_datetime(self.payload['enddate']).date()
+        })
 
     def _convert_to_df(self, list_of_dicts):
         df = pd.DataFrame(list_of_dicts).set_index('station')
@@ -81,61 +131,6 @@ class NoaaApi(object):
         else:
             print('WARNING: Data structure is empty.')
             return True
-
-    def _iteration_complete(self, data_):
-        df = self._convert_to_df(data_)
-        n_records = len(df)
-        most_recent_date = df.sort_values('date').iloc[-1].date.split('T')[0]
-        print('number of records: {n}'.format(n=n_records))
-        print('most recent record: {date}'.format(date=most_recent_date))
-        if (n_records < 1000) or (most_recent_date == self.payload['enddate']):
-            print('No more records to retrieve')
-            return True
-        else:
-            return False
-
-    def _iterate_over_pages(self, query_params):
-        for i in range(100):
-            self.payload.update({'offset': 1000*i})
-            response = self._make_request(query_params)
-            if self._valid_response(response):
-                data_ = self._parse_response(response)
-                self._insert_documents_into_db(data_)
-                if self._iteration_complete(data_):
-                    break
-                else:
-                    continue
-            else:
-                continue
-
-    # def _write_to_temp_file(self, data, filename='temp_data', extension='txt'):
-    #     assert isinstance(data, pd.DataFrame), 'expecting Pandas DataFrame'
-    #     filepath = "data/NOAA_{}.{}".format(filename, extension)
-    #     with open(filepath, 'a') as data_file:
-    #         for index, row in data.iterrows():
-    #             data_file.write(row.to_string())
-    #     print("{n} records written to temp file".format(n=len(data)))
-
-    def _iterate_over_years(self, query_params):
-        year_range = pd.date_range(
-        start=self.payload['startdate'],
-        end=self.payload['enddate'],
-        freq='12M'
-        )
-        for i, year in enumerate(year_range[:-1]):
-            self.payload.update(
-            {'startdate': year_range[i], 'enddate': year_range[i+1]}
-            )
-            self._iterate_over_pages(query_params)
-
-    def get_data(self, query_params):
-        if (self.payload['enddate'] - self.payload['startdate']).days >= 365:
-            self._iterate_over_years(query_params)
-        else:
-            self._iterate_over_pages(query_params)
-
-    def _valid_date_range(self, payload):
-        pass
 
     def _insert_documents_into_db(self, documents):
         collection = self._init_mongo_client()
