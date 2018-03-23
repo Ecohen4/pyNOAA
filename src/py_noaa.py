1254f649f2d65205e39eb6353c5f72b76a60f2a6
Ecohen4 <ec2977@columbia.edu>
2018-03-23 17:06:16 -0600

    final-ish working version. always more to do...

--- src/py_noaa.py
+++ src/py_noaa.py
@@ -2,6 +2,7 @@ import json
 import pymongo
 import requests
 import pandas as pd
+from datetime import datetime
 from pprint import pprint
 
 
@@ -10,7 +11,7 @@ class NoaaApi(object):
     Helper methods for working with NOAA's data API.
     See https://www.ncdc.noaa.gov/cdo-web/webservices/v2 for API documentation.
     '''
-    def __init__(self, api_key):
+    def __init__(self, api_key, query_params=None):
         '''
         First, request an API token: https://www.ncdc.noaa.gov/cdo-web/token
         Second, add your token to your local `~/.bash_profile` or `~/.bash_rc`:
@@ -19,11 +20,13 @@ class NoaaApi(object):
         '''
         self.headers = {'token': api_key}
         self.endpoint = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?"
-        self.payload = self._default_query_params()
+        self._set_payload(query_params)
+        self._init_mongo_client()
 
     def get_data(self, query_params=None):
         if query_params is not None:
             self._update_payload(query_params)
+            self._init_mongo_client()
         if (self.payload['enddate'] - self.payload['startdate']).days >= 365:
             self._iterate_over_years()
         else:
@@ -48,6 +51,7 @@ class NoaaApi(object):
             self._iterate_over_pages()
 
     def _iterate_over_pages(self, max_pages=100):
+        exception_count = 0
         for i in range(max_pages):
             print('requesting page {}...'.format(i))
             self.payload.update({'offset': 1000*i})
@@ -61,6 +65,10 @@ class NoaaApi(object):
                     continue
             else:
                 continue
+                exception_count += 1
+                if exception_count > 20:
+                    print('Too many excpetions... exiting')
+                    break
 
     def _make_request(self):
         response = requests.get(self.endpoint, headers=self.headers, params=self.payload)
@@ -101,6 +109,15 @@ class NoaaApi(object):
         else:
             return False
 
+    def _set_payload(self, query_params=None):
+        if query_params is None:
+            query_params = self._default_query_params()
+            print('WARNING: No query parameters provided... falling back on arbitrary defaults')
+        self.payload = query_params
+        self._convert_datetime_to_date_only()
+        print('query parameters set:')
+        pprint(self.payload)
+
     def _update_payload(self, query_params):
         self.payload.update(**query_params)
         self._convert_datetime_to_date_only()
@@ -111,6 +128,7 @@ class NoaaApi(object):
         'locationid': 'ZIP:80435',
         'startdate': pd.to_datetime('2013-10-06').date(),
         'enddate': pd.to_datetime('2016-11-11').date(),
+        'units': 'metric',
         'limit': 1000,
         'offset': 0
         }
@@ -133,35 +151,56 @@ class NoaaApi(object):
             return True
 
     def _insert_documents_into_db(self, documents):
-        collection = self._init_mongo_client()
         print('{} documents received'.format(len(documents)))
         print('inserting documents into mongodb...')
         document_count = 0
         for doc in documents:
             try:
-                collection.insert(doc)
+                self.collection.insert(doc)
                 document_count += 1
             except pymongo.errors.DuplicateKeyError:
                 print("duplicate record found... skipping...")
                 continue
         print('done. {} documents successfully inserted to MongoDB'.format(document_count))
 
-    def _init_mongo_client(self, collection_name='GSOM'):
+    def _init_mongo_client(self):
+        collection_name = self.payload['datasetid']
         client = pymongo.MongoClient()   # Initiate Mongo client
         db = client.NOAA                 # Access database
         collection = db[collection_name] # Access collection
-        return collection                # return collection pointer
+        self.collection = collection     # assign pointer to class attribute
 
+    def _debug_statement(self):
+        print("why am I broken?")
+
+    def _query_collection(query):
+        return self.collection.find(query)
+
+    def _convert_date_string_to_datetime_object(self,
+            attr="date",
+            date_format="%Y-%m-%d %H:%M:%S.%f"
+            ):
+        '''
+        convert date strings to ISO datetime objects.
+        attr is the column name which needs to be modified
+        date_format is the format of the string eg : "%Y-%m-%d %H:%M:%S.%f"
+        '''
+        for obj in self.collection.find():
+            if obj[attr]:
+                if type(obj[attr]) is not datetime:
+                    time = datetime.strptime(obj[attr], date_format)
+                    col.update({'_id':obj['_id']},{'$set':{attr : time}})
 
 
 if __name__=="__main__":
     import os
     api_key = os.environ['NOAA_API_KEY']
-    NOAA = NoaaApi(api_key)
     payload = {
-        'datasetid': 'GSOM',
-        'locationid': 'ZIP:80435',
-        'startdate': '2010-01-01',
-        'enddate': '2015-01-01'
-        }
-    NOAA.get_data(payload)
+    'datasetid': 'GHCND',
+    'locationid': 'FIPS:08117',
+    'startdate': pd.to_datetime('2015-12-31').date(),
+    'enddate': pd.to_datetime('2018-02-01').date(),
+    'units': 'metric'
+    }
+    NOAA = NoaaApi(api_key, payload)
+    NOAA.get_data()
